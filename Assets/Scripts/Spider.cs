954dd86923f67c4ef4b53005ac20b1b0d5d8d267
ValleyPenguin <valleypenguin@gmail.com>
2026-04-18 19:04:46 -0600

    Removed Unused Code

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -143,16 +143,6 @@ public class Spider : MonoBehaviour
             //startSpiderNavMeshMovement();
 
         }
-        /*else if (spiderStartedWalking)
-        {
-            if (!playerHealth.didPlayerDie)
-            {
-                agent.enabled = true;
-                rb.isKinematic = true;
-                //Debug.Log("Spider started chasing ya!");
-                //agent.SetDestination(playerTarget.position);
-            }
-        }*/
     }
 
     private void OnCollisionEnter(Collision collision)
